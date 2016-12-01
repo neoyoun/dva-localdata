bec729070fb29cff986b53d4f34ab6e5c98ccd82
neoyoun <453400438@qq.com>
2016-12-01 18:02:42 +0800

    初次踩坑,dispath还没走通

---
+++ src/components/ProductList.js
@@ -0,0 +1,32 @@
+import React,{PropTypes} from 'react';
+import { Table,Popconfirm,Button } from 'antd';
+
+const ProductList = ({ onDelete, products })=>{
+  const columns = [{
+    title:'Name',
+    dataIndex:'name'
+  },{
+    title:'Actions',
+    render:(text, record) => {
+      return (
+        <Popconfirm title="确认删除?" onconfirm={()=> onDelete(record.id)}>
+          <Button>Delete</Button>
+        </Popconfirm>
+        )
+    }
+  }];
+  return (
+    <Table
+      dataSource={products}
+      columns={columns}
+    />
+  );
+  //return (<Table dataSource={products} columns={columns} />)
+}
+
+ProductList.PropTypes={
+  onDelete:PropTypes.func.isRequired,
+  products:PropTypes.array.isRequire
+}
+
+export default ProductList
